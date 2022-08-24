562c723143a9a1c51d2d75e83c74e8803800bce9
JoseMLM <jmlopezmuro@gmail.com>
2022-08-24 18:59:55 +0000

    Progesión footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,116 +1,79 @@
 import React from "react";
+import { FaPizzaSlice } from "react-icons/fa";
 export const Footer = () => {
-	return (
+  return (
+    <footer className="text-center text-lg-start bg-light text-muted fixed-bottom">
+      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div className="me-5 d-none d-lg-block">
+          <span>Get connected with us on social networks:</span>
+        </div>
+        <div>
+          <a href="" className="me-4 text-reset">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="" className="me-4 text-reset">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="" className="me-4 text-reset">
+            <i className="fab fa-google"></i>
+          </a>
+          <a href="" className="me-4 text-reset">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="" className="me-4 text-reset">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href="" className="me-4 text-reset">
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
+      </section>
 
-		<footer className="text-center text-lg-start bg-light text-muted fixed-bottom">
+      <section className="">
+        <div className="container text-center text-md-start mt-5">
+          <div className="row mt-3">
+            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">
+                <FaPizzaSlice/> Localización 
+              </h6>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3037.453491523754!2d-3.7002924854345727!3d40.42095616330786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4229c00799ed5d%3A0xdb745a3936539667!2sPORTAL%2010%20-%20Pizzas%20Artesanas%F0%9F%8D%95!5e0!3m2!1ses!2ses!4v1661363301167!5m2!1ses!2ses"
+                width="400"
+                height="200"
+                style={{ border: "0" }}
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
 
-			<section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Contacto</h6>
+              <a href="https://g.page/Portal-10?share">
+                <i className="fas fa-home me-1"></i> Calle de Barbieri, 10
+                (Madrid)
+              </a>
+              <p>
+                <i className="fas fa-envelope me-3"></i>
+                info@example.com
+              </p>
+              <p>
+                <i className="fas fa-phone me-2"></i> 91 1458110
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
 
-				<div className="me-5 d-none d-lg-block">
-					<span>Get connected with us on social networks:</span>
-				</div>
-				<div>
-					<a href="" className="me-4 text-reset">
-						<i className="fab fa-facebook-f"></i>
-					</a>
-					<a href="" className="me-4 text-reset">
-						<i className="fab fa-twitter"></i>
-					</a>
-					<a href="" className="me-4 text-reset">
-						<i className="fab fa-google"></i>
-					</a>
-					<a href="" className="me-4 text-reset">
-						<i className="fab fa-instagram"></i>
-					</a>
-					<a href="" className="me-4 text-reset">
-						<i className="fab fa-linkedin"></i>
-					</a>
-					<a href="" className="me-4 text-reset">
-						<i className="fab fa-github"></i>
-					</a>
-				</div>
-
-			</section>
-
-			<section className="">
-				<div className="container text-center text-md-start mt-5">
-
-					<div className="row mt-3">
-
-						<div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-
-							<h6 className="text-uppercase fw-bold mb-4">
-								<i className="fas fa-gem me-3"></i>Company name
-							</h6>
-							<p>
-								Here you can use rows and columns to organize your footer content. Lorem ipsum
-								dolor sit amet, consectetur adipisicing elit.
-							</p>
-						</div>
-
-						<div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
-							<h6 className="text-uppercase fw-bold mb-4">
-								Products
-							</h6>
-							<p>
-								<a href="#!" className="text-reset">Angular</a>
-							</p>
-							<p>
-								<a href="#!" className="text-reset">React</a>
-							</p>
-							<p>
-								<a href="#!" className="text-reset">Vue</a>
-							</p>
-							<p>
-								<a href="#!" className="text-reset">Laravel</a>
-							</p>
-						</div>
-
-						<div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
-							<h6 className="text-uppercase fw-bold mb-4">
-								Useful links
-							</h6>
-							<p>
-								<a href="#!" className="text-reset">Pricing</a>
-							</p>
-							<p>
-								<a href="#!" className="text-reset">Settings</a>
-							</p>
-							<p>
-								<a href="#!" className="text-reset">Orders</a>
-							</p>
-							<p>
-								<a href="#!" className="text-reset">Help</a>
-							</p>
-						</div>
-
-						<div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
-							<h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-							<p><i className="fas fa-home me-3"></i> New York, NY 10012, US</p>
-							<p>
-								<i className="fas fa-envelope me-3"></i>
-								info@example.com
-							</p>
-							<p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
-							<p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
-						</div>
-
-					</div>
-
-				</div>
-			</section>
-
-
-
-			<div className="text-center p-4" style={{ background: "rgba(0, 0, 0, 0.05)" }}>
-				© 2021 Copyright:
-				<a className="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-			</div>
-
-		</footer>
-
-	);
+      <div
+        className="text-center p-4"
+        style={{ background: "rgba(0, 0, 0, 0.05)" }}
+      >
+        © 2022 Copyright:
+        <a className="text-reset fw-bold" href="/">
+          Portal 10
+        </a>
+      </div>
+    </footer>
+  );
 };
