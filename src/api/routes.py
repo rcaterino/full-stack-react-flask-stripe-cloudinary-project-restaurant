236c161e3615473403bf21a4cf16127e67c027a8
Ricardo Fabio Caterino Polini <ricardocaterino@gmail.com>
2022-08-27 10:17:28 +0000

    PRODUCT AND ADDRESS CLASSES CREATE

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,12 +2,19 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Category, Product, Addresses
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+# get all the people
+@api.route('/users', methods=['GET'])
+def getUsers():
+    people_query = User.query.all()
+    return jsonify(people_query), 200
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
@@ -16,3 +23,23 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+@api.route('/category', methods=['GET'])
+def getCategory():
+    category_query = Category.query.all()
+    return jsonify(category_query), 200
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+@api.route('/Product', methods=['GET'])
+def getProduct():
+    products_query = Product.query.all()
+    return jsonify(products_query), 200 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+@api.route('/Addresses', methods=['GET'])
+def getAddresses():
+    addresses_query = Addresses.query.all()
+    return jsonify(addresses_query), 200 
+
+
+
