cfad5f78b81951cbbf3c8b3e37990e3e9c8de566
Facundopiedrabuena <faculpda46@gmail.com>
2022-08-30 17:17:22 +0000

    endpoints get

--- src/api/routes.py
+++ src/api/routes.py
@@ -34,22 +34,54 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-#----------------------------------------------------------------------------------------------------------------------------------------------------------
-@api.route('/category', methods=['GET'])
-def getCategory():
-    category_query = Category.query.all()
-    return jsonify(category_query), 200
+
 #----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-@api.route('/Product', methods=['GET'])
+@api.route('/product', methods=['GET'])
 def getProduct():
     products_query = Product.query.all()
-    return jsonify(products_query), 200 
+    all_product = list(map(lambda x: x.serialize(), products_query))
+    return jsonify(all_product), 200 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------
-@api.route('/Addresses', methods=['GET'])
+@api.route('/addresses', methods=['GET'])
 def getAddresses():
     addresses_query = Addresses.query.all()
-    return jsonify(addresses_query), 200 
+    all_addresses = list(map(lambda x: x.serialize(), addresses_query))
+    return jsonify(all_addresses), 200 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+@api.route('/category', methods=['GET'])
+def getAllCategory():
+    category_query = Category.query.all()
+    all_category = list(map(lambda x: x.serialize(), category_query))
+    return jsonify(all_category), 200
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+# #get only one user in db
+@api.route('/category/<int:id>', methods=['GET'])
+def getCategory(id):
+    category_query = Category.query.get(id)
+    return jsonify(category_query.serialize())
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
 
+@api.route("/newcategory", methods=["POST"])
+def postCategory():
+    info_request = request.get_json()
+    category1 = Category(name=info_request["name"], id=info_request["id"])
+    db.session.add(category1)
+    db.session.commit()
+    return jsonify("categoria creada"), 200
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------  
+
+@api.route("/editcategory/<int:id>", methods=["PUT"])
+def putcategory(id):
+    info_request = request.get_json()
+    category1 = Category.query.get(id)
+    print(category1)
+    if category1 is None:
+        raise APIException('User not found', status_code=404)
+    if "name" in info_request:
+        category1.name = info_request["name"]    
+    db.session.commit()
+    return jsonify("categoria editada"),200
+#----------------------------------------------------------------------------------------------------------------------------------------------------------  
 
