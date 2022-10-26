a11087558ff78d06e08be6654f56505dcaedfda9
Facundopiedrabuena <faculpda46@gmail.com>
2022-10-26 12:52:06 +0000

    cambios dashboard

--- src/front/js/pages/easyrestaurant/dashboard.js
+++ src/front/js/pages/easyrestaurant/dashboard.js
@@ -42,19 +42,9 @@ export const Dashboard = () => {
                 <tr>
                   <th scope="row">1</th>
                   <td>Pedido Número:{order.order_id}</td>
+                  <td>En cocina</td>
                 </tr>
                 ))}
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td colspan="2">Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
               </tbody>
             </table>
             
